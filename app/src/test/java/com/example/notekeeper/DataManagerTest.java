e8d735a905d47947c74f8d3dbd54cb67aa3753c2
Laurent Decorps <ldecorps@gmail.com>
2021-04-25 20:10:55 +0100

    Working with Android Tools and Testing
    Dealing with a Failed Unit Test

--- app/src/test/java/com/example/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/example/notekeeper/DataManagerTest.java
@@ -20,9 +20,8 @@ public class DataManagerTest {
 
         NoteInfo compareNote = dm.getNotes().get(noteIndex);
 
-        assertEquals(compareNote.getCourse(), course);
-        assertEquals(compareNote.getTitle(), noteTitle);
-        assertEquals(compareNote.getText(), noteText);
-
+        assertEquals(course, compareNote.getCourse());
+        assertEquals(noteTitle, compareNote.getTitle());
+        assertEquals(noteText, compareNote.getText());
     }
 }
