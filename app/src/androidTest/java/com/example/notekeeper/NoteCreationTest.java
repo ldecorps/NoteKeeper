e798f3d59a95b3a4ee7145c8b385fced1a96d0f7
Laurent Decorps <ldecorps@gmail.com>
2021-05-02 00:27:14 +0100

    Taking a Deeper Look at Activity Interaction:
    Implementing test verification

--- app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
@@ -16,6 +16,8 @@ import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+
 import static org.hamcrest.Matchers.*;
 
 
@@ -41,11 +43,20 @@ public class NoteCreationTest {
         onView(withId(R.id.fab)).perform(click());
         onView(withId(R.id.spinner_courses)).perform(click());
         onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(
+                containsString(course.getTitle()))));
 
-        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle)).
+                check(matches(withText(noteTitle)));
         onView(withId(R.id.text_note_text)).perform(typeText(noteText),
                 ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.text_note_text)).check(matches(withText(noteText)));
 
         pressBack();
+        int noteIndex = sDataManager.getNotes().size()-1;
+        NoteInfo note = sDataManager.getNotes().get(noteIndex);
+        assertEquals(course, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
     }
 }
