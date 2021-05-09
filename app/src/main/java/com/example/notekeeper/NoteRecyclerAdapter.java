2b4f59dadf9ced65a93ff79e100b37fe348dd75c
Laurent Decorps <ldecorps@gmail.com>
2021-05-09 09:58:22 +0100

    Binding data to RecyclerView
    Creating and Constructing the RecyclerView Adapter

---
+++ app/src/main/java/com/example/notekeeper/NoteRecyclerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.notekeeper;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>{
+    private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
+
+    public NoteRecyclerAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(NoteRecyclerAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
