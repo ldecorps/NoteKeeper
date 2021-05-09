059efa304d113b1eba614f40eb1354fad51e6526
Laurent Decorps <ldecorps@gmail.com>
2021-05-09 10:09:11 +0100

    Binding data to RecyclerView
    View Management Within a Recycler Adapter

--- app/src/main/java/com/example/notekeeper/NoteRecyclerAdapter.java
+++ app/src/main/java/com/example/notekeeper/NoteRecyclerAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,7 +19,8 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list, parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
@@ -33,8 +35,13 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder
     {
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            mTextCourse = (TextView) itemView.findViewById(R.id.text_course);
+            mTextTitle = (TextView) itemView.findViewById(R.id.text_title);
         }
     }
 }
