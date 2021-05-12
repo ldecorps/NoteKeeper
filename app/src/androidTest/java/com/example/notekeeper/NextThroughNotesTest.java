65c57ee6ebbde4bd41ede519887b80274ea5ad52
Laurent Decorps <ldecorps@gmail.com>
2021-05-12 22:21:51 +0100

    Testing Menu, Navigation Drawer, and RecyclerView Behavior
    Testing Navigation Drawer and RecyclerView

--- app/src/androidTest/java/com/example/notekeeper/NextThroughNotesTest.java
+++ app/src/androidTest/java/com/example/notekeeper/NextThroughNotesTest.java
@@ -1,14 +1,21 @@
 package com.example.notekeeper;
 
 import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import java.util.List;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 public class NextThroughNotesTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -16,5 +23,15 @@ public class NextThroughNotesTest {
     @Test
     public void NextThroughNotesTest(){
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes));
+
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        List<NoteInfo> notes =DataManager.getInstance().getNotes();
+        int index = 0;
+        NoteInfo note = notes.get(index);
+        onView(withId(R.id.spinner_courses)).check(
+                matches(withSpinnerText(note.getCourse().getTitle())));
+        onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+        onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
     }
 }
