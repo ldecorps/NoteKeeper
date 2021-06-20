ada24840faa0f5aa14d02eb7e292aa05fd68d899
Laurent Decorps <ldecorps@gmail.com>
2021-06-20 21:51:13 +0100

    Doing More with SQLite Queries
    Specifying Qualified Column Names

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -207,17 +207,18 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
                 public Cursor loadInBackground() {
                     SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
                     final String[] noteColumns = {
-                            NoteInfoEntry._ID,
+                            NoteInfoEntry.getQName(NoteInfoEntry._ID),
                             NoteInfoEntry.COLUMN_NOTE_TITLE,
-                            NoteInfoEntry.COLUMN_COURSE_ID};
+                            NoteInfoEntry.getQName(NoteInfoEntry.COLUMN_COURSE_ID),
+                            CourseInfoEntry.COLUMN_COURSE_TITLE};
                     final String noteOrderBy = NoteInfoEntry.COLUMN_COURSE_ID +
                             "," + NoteInfoEntry.COLUMN_NOTE_TITLE;
 
                     // note_info join course_info
                     String tablesWithJoin = NoteInfoEntry.TABLE_NAME
                             + " JOIN " + CourseInfoEntry.TABLE_NAME
-                            + " ON " + NoteInfoEntry.TABLE_NAME + "." + NoteInfoEntry.COLUMN_COURSE_ID
-                            + " = " + CourseInfoEntry.TABLE_NAME + "." + CourseInfoEntry.COLUMN_COURSE_ID;
+                            + " ON " + NoteInfoEntry.getQName(NoteInfoEntry.COLUMN_COURSE_ID)
+                            + " = " + CourseInfoEntry.getQName(CourseInfoEntry.COLUMN_COURSE_ID);
                     return db.query(tablesWithJoin, noteColumns,
                             null, null, null, null, noteOrderBy);
                 }
--- app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
@@ -11,6 +11,10 @@ public final class NoteKeeperDatabaseContract implements BaseColumns {
         public static final String COLUMN_COURSE_TITLE = "course_title";
         public static final String _ID = "_id";
 
+        public static final String getQName(String columnName){
+            return TABLE_NAME + "." + columnName;
+        }
+
         // CREATE TABLE course_info (course_id, course_title)
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE "+TABLE_NAME+" ("+
@@ -27,6 +31,9 @@ public final class NoteKeeperDatabaseContract implements BaseColumns {
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String _ID = "_id";
 
+        public static final String getQName(String columnName){
+            return TABLE_NAME + "." + columnName;
+        }
 
         // CREATE TABLE course_info (course_id, course_title)
         public static final String SQL_CREATE_TABLE =
